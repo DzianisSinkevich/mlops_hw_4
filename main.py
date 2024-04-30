a348be61700df43689a500a2c34c1a3c0a55b246
Denis S <denis13th@gmail.com>
2024-04-30 18:43:43 +0300

    Hyrda added. Changes in main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from catboost.datasets import titanic
 import pandas as pd
 import os
-from sklearn.preprocessing import OneHotEncoder  # Импортируем One-Hot Encoding от scikit-learn
+from sklearn.preprocessing import OneHotEncoder
+import hydra
+from omegaconf import DictConfig, OmegaConf
+import logging
+log = logging.getLogger(__name__)
 
 
 def save_file(df, dir_path, file_name):
@@ -10,25 +14,26 @@ def save_file(df, dir_path, file_name):
     try:
         file_path = os.path.join(dir_path, file_name)
         df.to_csv(file_path, index=False)
-        print("File " + file_path + " created successfully.")
+        log.info("File " + file_path + " created successfully.")
     except IOError:
-        print("Error uccured while creating file " + file_path + " .")
+        log.error("Error uccured while creating file " + file_path + " .")
 
 
-def read_file(file_path):
+def read_file(dir_path, file_name):
     try:
+        file_path = os.path.join(dir_path, file_name)
         df = pd.read_csv(file_path)
 
-        print("File " + file_path + " readed successfully.")
+        log.info("File " + file_path + " readed successfully.")
         return df
     except IOError:
-        print("Error uccured while readed file '" + file_path + "'.")
+        log.error("Error uccured while readed file '" + file_path + "'.")
 
 
 # загрузка данных
 def read_dfs():
-    train = read_file('datasets/titanic_train.scv')
-    test = read_file('datasets/titanic_test.scv')
+    train = read_file('datasets', 'titanic_train.scv')
+    test = read_file('datasets', 'titanic_test.scv')
     return train, test
 
 
@@ -62,21 +67,47 @@ def one_hot(df, column_names):
     return df_ohe
 
 
-def main():
-    # train, test = titanic()  # 1
+def step_1():
+    log.info("Start Step #1.")
+    train, test = titanic()
+    save_dfs(train, test)
+    log.info("Finish Step #1.")
+
+
+def step_2():
+    log.info("Start Step #2.")
     train, test = read_dfs()
+    train, test = anti_none(train, test)
+    save_dfs(train, test)
+    log.info("Finish Step #2.")
 
-    # 2
-    # train, test = anti_none(train, test)
 
-    # 3
-    # train, test = sex_convert(train, test)
+def step_3():
+    log.info("Start Step #3.")
+    train, test = read_dfs()
+    train, test = sex_convert(train, test)
+    save_dfs(train, test)
+    log.info("Finish Step #3.")
 
-    # 4
+
+def step_4():
+    log.info("Start Step #4.")
+    train, test = read_dfs()
     train = pd.concat([train, one_hot(train, ['Sex'])], axis=1).reindex(train.index)
     test = pd.concat([test, one_hot(test, ['Sex'])], axis=1).reindex(test.index)
-
     save_dfs(train, test)
+    log.info("Finish Step #4.")
 
 
-main()
+@hydra.main(version_base=None, config_path='config', config_name='config')
+def main(cfg: DictConfig) -> None:
+    print(OmegaConf.to_yaml(cfg))
+
+    step_1()
+    step_2()
+    step_3()
+    step_4()
+
+
+if __name__ == "__main__":
+    main()
